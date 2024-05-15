251708f3cc597c53c5ea0decb8545166bad584f5
haminh0307 <haminh0307@gmail.com>
2024-05-15 15:33:20 +0700

    feat: add binding tag in user entities

--- internal/domain/entity/user.go
+++ internal/domain/entity/user.go
@@ -11,14 +11,14 @@ type User struct {
 type UserToCreate struct {
 	BaseToCreate `bson:"inline"`
 
-	Username     string `json:"username" bson:"username"`
+	Username     string `json:"username" bson:"username" binding:"required"`
 	PasswordHash []byte `json:"-" bson:"passwordHash"`
-	Password     string `json:"password" bson:"-"`
-	FullName     string `json:"fullName" bson:"fullName"`
+	Password     string `json:"password" bson:"-" binding:"required"`
+	FullName     string `json:"fullName" bson:"fullName" binding:"required"`
 }
 
 type UserToUpdate struct {
 	UpdatedAt TimeNow `json:"-" bson:"updated_at"`
-	Username  string  `json:"username" bson:"username,omitempty"`
-	FullName  string  `json:"fullName" bson:"fullName,omitempty"`
+	Username  string  `json:"username" bson:"username,omitempty" binding:"required_without=FullName"`
+	FullName  string  `json:"fullName" bson:"fullName,omitempty" binding:"required_without=Username"`
 }
